833023a4f471a5ec9d608482af1596d40b08048f
ksmode <seu-email@example.com>
2025-03-21 16:01:29 -0300

    ksmodee

--- includes/colete2.0.js
+++ includes/colete2.0.js
@@ -1,6 +1,7 @@
 (function () {
     "use strict";
-  
+    
+    console.log("By ksmode")
     let cltEnabled = false;
     const [iconId, cltSlot] = ["cltIcon", document.getElementById("ehp2")];
   
