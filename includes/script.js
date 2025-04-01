4c3cb573fc3e2a9558c406e143dc98088b09f3dc
Your Name <you@example.com>
2025-04-01 14:19:09 -0300

    up

--- includes/script.js
+++ includes/script.js
@@ -577,17 +577,71 @@ function handlekeyup(e){
 }*/
 
 //orientaçao a ojetos
-
+/*
 class Person {
-    constructor(name, age){
-        this.name = name;
-        this.age = age;
+    _age = 10;
+    steps = 0;
+    constructor(firstName, lastName){
+       this.firstName = firstName;
+       this.lastName = lastName;
+    }
+
+    takeAStep (){
+        this.steps++;
+    }
+    get fullname(){
+        return `${this.firstName} ${this.lastName}`;
+
+    }
+    get age(){
+        return this._age;
+
+    }
+    set age(x) {
+        if (typeof x === 'number') {
+            this._age = x;
+        }
     }
 }
 
-let p1 = new Person("Ksmode", 18);
-let p2 = new Person("Maria", 30);
-let p3 = new Person("Pedro", 40);
 
-console.log(p1.age);
+let p1 = new Person("Ksmode", "Oliveira");
+let p2 = new Person("Maria", "kaka");
+let p3 = new Person("Pedro", "ox");
 
+ p1._age = 'abc';
+console.log(`${p1.fullname}: Tem ${p1.age} anos `);
+
+
+
+
+/*
+console.log(`P1 = ${p1.name} tem ${p1.age}`);
+console.log(`P2 = ${p1.name} tem ${p1.age}`);
+console.log(`P3 = ${p1.name} tem ${p1.age}`);
+
+p1.takeAStep();
+p1.takeAStep();
+console.log(`Passos de ${p1.name}: ${p1.steps}`);
+console.log(`Passos de ${p2.name}: ${p2.steps}`);
+*/
+
+class Person {
+
+    age = 0;
+
+    constructor(name){
+       this.name = name;
+    //    this.age = age;
+    }
+}
+
+let p1 = new Person("Ksmode");
+let p2 = new Person("Maria");
+let p3 = new Person("Pedro");
+
+p1.age = 20;
+
+console.log(`P1 = ${p1.name} tem ${p1.age}`);
+console.log(`P1 = ${p2.name} tem ${p2.age}`);
+console.log(`P1 = ${p3.name} tem ${p3.age}`);
